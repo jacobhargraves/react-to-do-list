2d6f534d72393fc30e2d90dfd79e824683344d2b
Jacob Hargraves <jacobhargraves1@gmail.com>
2023-09-20 11:32:01 -0600

    added queries for client side

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -2,11 +2,32 @@ import { gql } from '@apollo/client';
 
 export const QUERY_USER = gql`
   query user($username: String!) {
-    
+    user(username: $username) {
+      _id
+      username
+      email
+      todos {
+        _id
+        item
+        completed
+        createdAt
+      }
+    }
   }
 `;
 
 export const QUERY_ME = gql`
   query me {
+    me {
+      _id
+      username
+      email
+      todos {
+        _id
+        item
+        completed
+        createdAt
+      }
+    }
   }
 `;
