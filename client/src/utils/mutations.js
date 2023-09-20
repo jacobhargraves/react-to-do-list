5d36f78f9fcbf9c17ad5492f6e42f8a0158512c0
Jacob Hargraves <jacobhargraves1@gmail.com>
2023-09-20 11:29:45 -0600

    added mutations for client side

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,13 +1,43 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password){
+      token
+      user {
+          _id
+          username
+      }
+    }
+  }
 `;
 
 export const ADD_USER = gql`
-
+  mutation addUser($username: String!, $email: String!, $password: String!){
+    addUser(username: $username, email: $email, password: $password){
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
 `;
 
 export const ADD_TODO = gql`
-
+  mutation addTodo ($userId: ID!, $item: String!){
+    addTodo (userId: $userId, item: $item){
+      _id
+      username
+      email
+      todos {
+        _id
+        item
+        completed
+        createdAt
+      }
+    }
+  }
 `;
+
+// code to delete todo item
